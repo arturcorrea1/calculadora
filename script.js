e997f90eec3ba7d71aef8e18f57d87d10d880439
arturcorrea1 <126278572+arturcorrea1@users.noreply.github.com>
2024-05-25 03:00:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// definição de variaveis
 let display = document.getElementById("display");
 let valorAnterior = '';
 let operacao = '';
 
+// aparecer numero no display
+
 function adicionar(caractere) {
+    // botão c pra remover todos os caracteres
     if (caractere === 'C') {
         display.innerHTML = '0';
         valorAnterior = '';
@@ -11,10 +15,13 @@ function adicionar(caractere) {
         if (display.innerHTML === '0') {
             display.innerHTML = caractere;
         } else {
-            display.innerHTML += caractere
+            display.innerHTML += caractere 
+            // colocar mais de um numero
         }
     }
 }
+
+// calculo das operações com os dados recebidos
 
 function calcular() {
     try {
